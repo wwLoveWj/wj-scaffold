4dcc6a9b3c06c966a2a2dbb7ede1333de7fd0967
wwLoveWj <blww885@163.com>
2025-03-13 15:42:01 +0800

    feat: 文件的copy

--- bin/copy.js
+++ bin/copy.js
@@ -1,8 +1,26 @@
 const copydir = require("copy-dir");
 const fs = require("fs");
+const path = require("path");
 
-function copyDir(from, to, options) {
-  copydir.sync(from, to, options);
+function mkdirGuard(target) {
+  try {
+    fs.mkdirSync(target, { recursive: true });
+  } catch (e) {
+    mkdirp(target);
+    function mkdirp(dir) {
+      if (fs.existsSync(dir)) {
+        return true;
+      }
+      const dirname = path.dirname(dir);
+      mkdirp(dirname);
+      fs.mkdirSync(dir);
+    }
+  }
+}
+
+function copyDir(form, to, options) {
+  mkdirGuard(to);
+  copydir.sync(form, to, options);
 }
 
 function checkMkdirExists(path) {
@@ -10,4 +28,5 @@ function checkMkdirExists(path) {
 }
 
 exports.checkMkdirExists = checkMkdirExists;
+exports.mkdirGuard = mkdirGuard;
 exports.copyDir = copyDir;
--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 const yargs = require("yargs");
+const path = require("path");
 const { inquirerPrompt } = require("./inquirer");
+const { copyDir, checkMkdirExists } = require("./copy");
 
 yargs.command(
   ["create", "c"],
@@ -15,9 +17,20 @@ yargs.command(
     });
   },
   function (argv) {
-    console.log("argv--------------", argv);
     inquirerPrompt(argv).then((answers) => {
-      console.log(answers);
+      const { name, type } = answers;
+      console.log(type, name, "form-------");
+      const isMkdirExists = checkMkdirExists(
+        path.resolve(process.cwd(), `./src/pages/${name}`)
+      );
+      if (isMkdirExists) {
+        console.log(`${name}文件夹已经存在`);
+      } else {
+        copyDir(
+          path.resolve(__dirname, `./template/${type}`),
+          path.resolve(process.cwd(), `./src/pages/${name}`)
+        );
+      }
     });
   }
 ).argv;
