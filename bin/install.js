968296396f58beef48ab0458615989dcbe89965e
wwLoveWj <blww885@163.com>
2025-03-13 17:21:55 +0800

    feat: 增加ora

--- bin/install.js
+++ bin/install.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const { exec } = require("child_process");
+const ora = require("ora");
 
 const LibraryMap = {
   "Ant Design": "antd",
@@ -12,15 +13,21 @@ function install(cmdPath, options) {
   const { frame, library } = options;
   const command = `yarn add ${frame} && yarn add ${LibraryMap[library]}`;
   return new Promise(function (resolve, reject) {
+    const spinner = ora();
+    spinner.start(`正在安装依赖，请稍等`);
     exec(
       command,
       {
         cwd: path.resolve(cmdPath),
       },
-      function (error, stdout, stderr) {
-        console.log("error", error);
-        console.log("stdout", stdout);
-        console.log("stderr", stderr);
+      function (error) {
+        if (error) {
+          reject();
+          spinner.fail(`依赖安装失败`);
+          return;
+        }
+        spinner.succeed(`依赖安装成功`);
+        resolve();
       }
     );
   });
