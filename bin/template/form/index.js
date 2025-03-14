f907775b2db23d5bad265857aaafab755f5a3451
wwLoveWj <blww885@163.com>
2025-03-14 14:01:04 +0800

    feat: 实现项目下载及antd等库的下载

--- bin/template/form/index.js
+++ bin/template/form/index.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Button, Form, Input } from "antd";
 
-export default function Index() {
-  return <div>index</div>;
-}
+const App = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  return (
+    <Form onFinish={onFinish} autoComplete="off">
+      <Form.Item label="Username" name="username">
+        <Input />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          提交
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+export default App;
